2461a633900f6e5e82c87e424534167d98d0013d
Kate-Zilla <equineglory@gmail.com>
2024-09-18 20:19:53 -0700

    other modules

---
+++ primemodule.py
@@ -0,0 +1,21 @@
+def is_prime(n):
+    if n<=1:
+        return False
+    for i in range(2, int(n**.5)+1):
+        if n % i == 0:
+            return False
+        
+    return True
+
+
+def print_primes(n):
+    for num in range (2,n):
+        if is_prime(num):
+            print(num, end = '  ')
+
+def get_primes(n):
+    prime_lst = []
+    for num in range (2,n):
+        if is_prime(num):
+            prime_lst.append(str(num))
+    return prime_lst
